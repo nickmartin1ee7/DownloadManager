a4a2f367206dd83547794d97769533d2cc83baed
Nicholas M Martin <nmmart04@louisville.edu>
2022-05-07 01:38:43 -0400

    Add save location btn

--- DownloadManager/ViewModels/MainWindowViewModel.cs
+++ DownloadManager/ViewModels/MainWindowViewModel.cs
@@ -1,13 +1,46 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using System.Collections.ObjectModel;
-using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace DownloadManager.ViewModels
 {
     public class MainWindowViewModel
     {
-        public ObservableCollection<JobReportModel> Jobs { get; set; }
+        private StringBuilder _sb = new();
+        private FileInfo? _saveLocation;
+
+        public ObservableCollection<JobReportModel> Jobs { get; } = new();
+
+        public ICommand AddCommand { get; }
+
+        public ICommand ChangeSaveLocationCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            AddCommand = new Command(Add);
+            ChangeSaveLocationCommand = new Command(ChangeSaveLocation);
+        }
+
+        private void Add(object? _)
+        {
+
+        }
+
+
+        private void ChangeSaveLocation(object? saveLocationPath)
+        {
+            var path = saveLocationPath as string;
+
+            if (!File.Exists(path)) return;
+
+            _saveLocation = new FileInfo(path);
+        }
+
+        private void Log(string message)
+        {
+            _sb.AppendLine($"[{DateTime.Now}] {message}");
+        }
     }
 }
